f6ceec4a75f296d11b27751567dadbcae0cdb31f
LAPTOP-P0FNKAG7\Paul <basiclick@gmail.com>
2016-10-08 10:47:19 -0600

    Add project files.

---
+++ CS4790Assignment4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CS4790Assignment4.MvcApplication" Language="C#" %>
---
+++ CS4790Assignment4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CS4790Assignment4.Startup))]
+namespace CS4790Assignment4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
